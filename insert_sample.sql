022fb4a12d2be898acd774712b625993e79fb7f8
kocya777 <114490829+kocya777@users.noreply.github.com>
2025-03-19 17:54:16 +0900

    Add files via upload

--- insert_sample.sql
+++ insert_sample.sql
@@ -1,4 +1,4 @@
 delete from user_id;
 insert into user_id values (NULL, 'fujiwara', 'nice4970', 'takahashi', now());
 delete from customer_master;
-insert into customer_master values (NULL, '藤原浩一', 'フジワラコウイチ', '岡山県倉敷市中島715-1', 59, '0864600610', '09028052411', 'fujiwara@mi-kuni.jp', 'テスト登録', 'takahashi', now());
+insert into customer_master values (NULL, '織田裕二', 'オダユウジ', '東京都港区麻布十番1-2-3', 42, '0322223333', '09022223333', 'u.oda@gmail.co.jp', 'テスト登録', 'takahashi', now());
